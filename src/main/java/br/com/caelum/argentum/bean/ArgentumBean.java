fb80601298f6bc56677293a347162099ebed1ccc
nog <nog.guitar@gmail.com>
2019-03-02 20:43:24 -0300

    Teste de commit quebrado

--- src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
+++ src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
@@ -40,99 +40,99 @@ public class ArgentumBean implements Serializable {
 		geraGrafico();
 	}
 
-	public void geraGrafico() {
-		List<Candle> candles = new CandlestickFactory().constroiCandles(negociacoes);
-		SerieTemporal serie = new SerieTemporal(candles);
-		GeradorModeloGrafico geradorGrafico = new GeradorModeloGrafico(serie, 2, serie.getUltimaPosicao());
-		geradorGrafico.plotaMediaMovelSimples(defineIndicador());
-		this.modeloGrafico = geradorGrafico.getModeloGrafico();
-	}
-
-	public void filtra() {
-		aplicaFiltro();
-		geraGrafico();
-	}
-
-	private void aplicaFiltro() {
-		Calendar de = Calendar.getInstance();
-		if (filtroDataDe != null) {
-			de.setTime(filtroDataDe);
-		} else {
-			de.add(Calendar.MONTH, -12);
-		}
-		
-		Calendar ate = Calendar.getInstance();
-		if (filtroDataAte != null) {
-			ate.setTime(filtroDataAte);
-		} else {
-			ate.add(Calendar.MONTH, 12);
-		}
-		
-		negociacoes = new ClienteWebService().getNegociacoes();
-		
-		negociacoes = negociacoes
-						.stream()
-						.filter(n -> n.getData().after(de) && n.getData().before(ate))
-						.collect(Collectors.toList());
-	}
-	
-	private Indicador defineIndicador() {
-		if (nomeIndicadorBase == null || nomeIndicadorBase.isEmpty() || nomeMedia == null || nomeMedia.isEmpty()) {
-			return new MediaMovelSimples(new IndicadorFechamento());
-		}
-
-		String pacote = "br.com.caelum.argentum.indicadores.";
-		try {
-			Class<?> classeIndicadorBase = Class.forName(pacote + nomeIndicadorBase);
-			Indicador indicadorBase = (Indicador) classeIndicadorBase.newInstance();
-
-			Class<?> classeMedia = Class.forName(pacote + nomeMedia);
-			Constructor<?> construtorMedia = classeMedia.getConstructor(Indicador.class);
-			Indicador indicador = (Indicador) construtorMedia.newInstance(indicadorBase);
-			return indicador;
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
-	}
-
-	public ChartModel getModeloGrafico() {
-		return modeloGrafico;
-	}
-
-	public List<Negociacao> getNegociacoes() {
-		return negociacoes;
-	}
-
-	public String getNomeIndicadorBase() {
-		return nomeIndicadorBase;
-	}
-
-	public String getNomeMedia() {
-		return nomeMedia;
-	}
-
-	public void setNomeIndicadorBase(String nomeIndicadorBase) {
-		this.nomeIndicadorBase = nomeIndicadorBase;
-	}
-
-	public void setNomeMedia(String nomeMedia) {
-		this.nomeMedia = nomeMedia;
-	}
-
-	public Date getFiltroDataDe() {
-		return filtroDataDe;
-	}
-
-	public void setFiltroDataDe(Date filtroDataDe) {
-		this.filtroDataDe = filtroDataDe;
-	}
-
-	public Date getFiltroDataAte() {
-		return filtroDataAte;
-	}
-
-	public void setFiltroDataAte(Date filtroDataAte) {
-		this.filtroDataAte = filtroDataAte;
-	}
+//	public void geraGrafico() {
+//		List<Candle> candles = new CandlestickFactory().constroiCandles(negociacoes);
+//		SerieTemporal serie = new SerieTemporal(candles);
+//		GeradorModeloGrafico geradorGrafico = new GeradorModeloGrafico(serie, 2, serie.getUltimaPosicao());
+//		geradorGrafico.plotaMediaMovelSimples(defineIndicador());
+//		this.modeloGrafico = geradorGrafico.getModeloGrafico();
+//	}
+//
+//	public void filtra() {
+//		aplicaFiltro();
+//		geraGrafico();
+//	}
+//
+//	private void aplicaFiltro() {
+//		Calendar de = Calendar.getInstance();
+//		if (filtroDataDe != null) {
+//			de.setTime(filtroDataDe);
+//		} else {
+//			de.add(Calendar.MONTH, -12);
+//		}
+//		
+//		Calendar ate = Calendar.getInstance();
+//		if (filtroDataAte != null) {
+//			ate.setTime(filtroDataAte);
+//		} else {
+//			ate.add(Calendar.MONTH, 12);
+//		}
+//		
+//		negociacoes = new ClienteWebService().getNegociacoes();
+//		
+//		negociacoes = negociacoes
+//						.stream()
+//						.filter(n -> n.getData().after(de) && n.getData().before(ate))
+//						.collect(Collectors.toList());
+//	}
+//	
+//	private Indicador defineIndicador() {
+//		if (nomeIndicadorBase == null || nomeIndicadorBase.isEmpty() || nomeMedia == null || nomeMedia.isEmpty()) {
+//			return new MediaMovelSimples(new IndicadorFechamento());
+//		}
+//
+//		String pacote = "br.com.caelum.argentum.indicadores.";
+//		try {
+//			Class<?> classeIndicadorBase = Class.forName(pacote + nomeIndicadorBase);
+//			Indicador indicadorBase = (Indicador) classeIndicadorBase.newInstance();
+//
+//			Class<?> classeMedia = Class.forName(pacote + nomeMedia);
+//			Constructor<?> construtorMedia = classeMedia.getConstructor(Indicador.class);
+//			Indicador indicador = (Indicador) construtorMedia.newInstance(indicadorBase);
+//			return indicador;
+//		} catch (Exception e) {
+//			throw new RuntimeException();
+//		}
+//	}
+//
+//	// public ChartModel getModeloGrafico() {
+//	// 	return modeloGrafico;
+//	// }
+//
+//	// public List<Negociacao> getNegociacoes() {
+//	// 	return negociacoes;
+//	// }
+//
+//	// public String getNomeIndicadorBase() {
+//	// 	return nomeIndicadorBase;
+//	// }
+//
+//	// public String getNomeMedia() {
+//	// 	return nomeMedia;
+//	// }
+//
+//	// public void setNomeIndicadorBase(String nomeIndicadorBase) {
+//	// 	this.nomeIndicadorBase = nomeIndicadorBase;
+//	// }
+//
+//	// public void setNomeMedia(String nomeMedia) {
+//	// 	this.nomeMedia = nomeMedia;
+//	// }
+//
+//	// public Date getFiltroDataDe() {
+//	// 	return filtroDataDe;
+//	// }
+//
+//	// public void setFiltroDataDe(Date filtroDataDe) {
+//	// 	this.filtroDataDe = filtroDataDe;
+//	// }
+//
+//	// public Date getFiltroDataAte() {
+//	// 	return filtroDataAte;
+//	// }
+//
+//	// public void setFiltroDataAte(Date filtroDataAte) {
+//	// 	this.filtroDataAte = filtroDataAte;
+//	// }
 
 }
